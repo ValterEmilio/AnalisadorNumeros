532f5337f6baefcefd960a4786c5465832342238
ValterEmilio <valternic77@gmail.com>
2025-08-21 07:45:49 -0300

    criando nova aplicação

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function tabuada(){
-    //declaração de variaveis
-    let numero = document.getElementById('numero');
-    let gerar = document.getElementById('criaTabuada');
-    //verifica se o campo esta vazio
-    if ( numero.value.length == 0) {
-        window.alert('ERROR digite um numero!');
-    } else {
-        let n = Number(numero.value);
-        let contador = 1
-        gerar.innerHTML = '' //limpa o select antes de mostrar outra tabuada
-        while ( contador <= 10) {
-            let item = document.createElement('option')//cria um elemento dentro do select no html
-            item.text = `${n} X ${contador} = ${n*contador}`//cria a tabuada enquanto contador for verdadeiro
-            item.value = `gerar${contador}`
-            gerar.appendChild(item)
-           
-            contador++
-        }
-        numero.value = ''
-    }
+function adicionar(){
+    let cx = document.getElementById('caixa')
 }
