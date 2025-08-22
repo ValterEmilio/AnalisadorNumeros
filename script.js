5450f3733de89cb2b9e27ea4c22b91cb2cafb791
ValterEmilio <valternic77@gmail.com>
2025-08-22 07:57:42 -0300

    adicionando numeros

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function adicionar(){
     let cx = document.getElementById('caixa')
+    let num = document.getElementById('numeros')
+    if (cx.value.length === 0){
+        window.alert('Digite um numero!')
+    } else {
+        num.value = ''
+        let n = Number(cx.value)
+        let item = document.createElement('option')//cria um elemento dentro do select no html
+            item.text = `numero ${n} adicionado`//cria a tabuada enquanto contador for verdadeiro
+            item.value = `gerar${cx}`
+            num.appendChild(item) 
+    }
+    
 }
