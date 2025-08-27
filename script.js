57d3b20a6e77a431c1e975a75dab8f2b8f09685b
ValterEmilio <valternic77@gmail.com>
2025-08-27 06:51:01 -0300

    finalizado aplicação

--- script.js
+++ script.js
@@ -4,22 +4,27 @@ let res = document.querySelector("div#resultado")
 let valores = []
 
 function isNumero(n) {
-    return Number(n) >= 1 && Number(n) <= 100;
+    //return Number(n) >= 1 && Number(n) <= 100;
+    return n >= 1 && n <= 100
 }
 function inLista(n, l){
-    return l.includes(Number(n));
+    //return l.includes(Number(n));
+    return l.includes(n)
 
 }
 function adicionar(){
-    if (isNumero(num.value) && !inLista(num.value, valores)){
+    let n = Number(num.value)
 
-        valores.push(Number(num.value))
+    if (isNumero(n) && !inLista(n , valores)){
+
+        valores.push(n)
         let item = document.createElement('option')
         item.text = `valor ${num.value} adicionado`
         cx.appendChild(item)
+        res.innerHTML = ''
 
     } else {
-        window.alert("Digite um numero valido!")
+        alert("Digite um numero valido!")
     }
     num.value = ''
     num.focus()
@@ -30,9 +35,17 @@ function analisar() {
         window.alert('Adicione valores!')
     } else {
         let total = valores.length
+        let maior = Math.max(...valores)
+        let menor = Math.min(...valores)
+        let soma = valores.reduce((a, b) => a + b, 0)
+        let media = soma / total
 
         res.innerHTML = ''
-        res.innerHTML    =       
+        res.innerHTML += `<p> ${total} valores adicionados </p> ` 
+        res.innerHTML += `<p> ${maior} é o maior numero</p>`
+        res.innerHTML += `<p> ${menor} é o menor numero</p>`
+        res.innerHTML += `<p> a Soma dos valores é ${soma}</p>`
+        res.innerHTML += `<p> A media dos valores é ${media}</p>`       
     }
 
 }      
